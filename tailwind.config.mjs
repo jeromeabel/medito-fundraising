dc797130e8eb170e531e0aa83965fb1276a43252
Jérôme Abel <contact@jeromeabel.net>
2024-01-15 15:50:36 +0100

    css: center container

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        black: '#151515',
+        white: '#fdfdfd',
+      },
+      container: {
+        center: true,
+        padding: '2rem',
+      },
+    },
+  },
+  plugins: [],
+};
