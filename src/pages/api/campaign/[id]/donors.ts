d9204068ec942f24fcbf7ecc383070ca2968a11d
Jérôme Abel <contact@jeromeabel.net>
2024-01-17 13:53:55 +0100

    feat: add API endpoint for donors and fist calls in CampaignProgress

---
+++ src/pages/api/campaign/[id]/donors.ts
@@ -0,0 +1,25 @@
+import type { APIRoute } from 'astro';
+import { supabase } from '@config/supabase';
+
+export const GET: APIRoute = async ({ params }) => {
+  const campaignId = params.id;
+
+  const { data } = await supabase
+    .from('donors')
+    .select('*')
+    .eq('campaign_id', Number(campaignId));
+
+  if (!data) {
+    return new Response(null, {
+      status: 404,
+      statusText: 'Not found',
+    });
+  }
+
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
